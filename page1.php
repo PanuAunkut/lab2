dff569a0f1b5b8700d9e8cc162f7f0e5d83c45c3
PanuAunkut <panu.a@ku.th>
2023-01-20 17:40:58 +0700

    f2

--- page1.php
+++ page1.php
@@ -2,8 +2,13 @@
 <html>
 <body>
 <form action="show.php" method="post">
-    Name:<input type="text" name="name"><br>
-    Email:<input type="text"name="email"><br>
+    ชื่อ:<input type="text" name="name"><br>
+    อายุ:<input type="var"name="old">
+    เพศ:<input type="text"name="Sex"><br>
+    สถานะ:
+    <input type="radio"name="S" value="แต่งงานแล้ว">แต่งงานแล้ว
+    <input type="radio"name="S" value="โสด">โสด
+    <br>
     <input type="submit" value="send"> 
 </body>
 </html>
